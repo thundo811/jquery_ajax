087d9514590d66961960303c026f94ee6feddc12
thuando <gvnnino811@gmail.com>
2019-03-17 15:33:28 +0700

    abc

--- app/models/KhoaHoc.js
+++ app/models/KhoaHoc.js
@@ -1,8 +1,5 @@
 function NguoiDung(taiKhoan, matKhau, hoTen, email, soDT, maLoaiNguoiDung){
     this.taiKhoan = taiKhoan;
     this.matKhau = matKhau;
-    this.hoTen = hoTen;
-    this.email = email;
-    this.soDT = soDT;
-    this.maLoaiNguoiDung = maLoaiNguoiDung;
+
 }
